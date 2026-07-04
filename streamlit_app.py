e78fa8738268cf294bd175f29f91db460d612581
monicanixia <laurensiusden@gmail.com>
2026-07-04 22:26:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1336,37 +1336,37 @@ sentimen menggunakan **XGBoost** dan **LSTM**.
 
     if os.path.exists(nama_file):
 
-    st.markdown("### 🖼️ Confusion Matrix")
+        st.markdown("### 🖼️ Confusion Matrix")
 
-    # Membuat gambar berada di tengah
-    col1, col2, col3 = st.columns([1, 3, 1])
+        # Gambar berada di tengah
+        col1, col2, col3 = st.columns([1, 3, 1])
 
-    with col2:
-        st.image(
-            nama_file,
-            width=650,
-            caption=f"{model_pilih} | {dataset_pilih} | Split {split_pilih}"
-        )
+        with col2:
+            st.image(
+                nama_file,
+                width=600,
+                caption=f"{model_pilih} | {dataset_pilih} | Split {split_pilih}"
+            )
 
-    with open(nama_file, "rb") as img:
+        with open(nama_file, "rb") as img:
 
-        st.download_button(
-            label="⬇️ Download Confusion Matrix",
-            data=img,
-            file_name=nama_file,
-            mime="image/png"
-        )
+            st.download_button(
+                label="⬇️ Download Confusion Matrix",
+                data=img,
+                file_name=nama_file,
+                mime="image/png"
+            )
 
-    else:
+        else:
 
-        st.error(f"File tidak ditemukan : {nama_file}")
+            st.error(f"File tidak ditemukan : {nama_file}")
 
-        st.divider()
+            st.divider()
 
-        st.subheader("📈 Interpretasi Singkat")
+            st.subheader("📈 Interpretasi Singkat")
 
-        st.info(
-        f'''
+            st.info(
+            f'''
 Model **{model_pilih}** pada dataset **{dataset_pilih}**
 dengan pembagian data **{split_pilih}**
 ditampilkan melalui confusion matrix di atas.
